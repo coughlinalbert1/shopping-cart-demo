9acfbc9d42566d43226a1721ded56fe8deabb4fa
coughlinalbert1 <111651444+coughlinalbert1@users.noreply.github.com>
2024-12-03 01:13:24 -0500

    Add files via upload

--- src/com/shashi/service/TransService.java
+++ src/com/shashi/service/TransService.java
@@ -1,6 +1,14 @@
 package com.shashi.service;
 
+import com.shashi.beans.TransactionBean;
+
 public interface TransService {
 
 	public String getUserId(String transId);
+
+	public TransactionBean applyDiscount(TransactionBean transaction, String discountCode);
+
+	public boolean isValidDiscountCode(String discountCode);
+
+	public void recordTransaction(TransactionBean transaction) throws Exception;
 }
